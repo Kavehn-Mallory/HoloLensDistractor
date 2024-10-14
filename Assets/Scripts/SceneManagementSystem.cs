fc6a3e39ede4c440ec22e2250baa9e45a09368a8
Jan W. <JanWittke97@web.de>
2024-10-14 16:17:10 +0200

    working on client/server communication

---
+++ Assets/Scripts/SceneManagementSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Tymski;
+using Unity.Collections;
+using Unity.Networking.Transport.Samples;
+using UnityEngine;
+
+namespace DistractorProject
+{
+    public class SceneManagementSystem : MonoBehaviour
+    {
+
+        [SerializeField]
+        private ServerBehaviour serverBehaviour;
+        
+        [SerializeField]
+        private SceneReference[] sceneReferences;
+
+        
+
+
+        private void OnEnable()
+        {
+            serverBehaviour.OnDataStreamReceived += OnDataStreamReceived;
+        }
+
+        private void OnDisable()
+        {
+            serverBehaviour.OnDataStreamReceived -= OnDataStreamReceived;
+        }
+
+        private void OnDataStreamReceived(DataStreamReader obj)
+        {
+            Debug.Log("Scene managment informed");
+        }
+    }
+}
